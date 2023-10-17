dc07c08a34d11a9474903846ae2b22dabd1fbc8e
ethon-wang <ethon.wang@quectel.com>
2023-10-17 16:23:37 +0800

    plantClass

---
+++ proj0/Planet.java
@@ -0,0 +1,41 @@
+import javax.crypto.interfaces.PBEKey;
+import javax.xml.xpath.XPath;
+
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+
+    public double mass;
+    public String imgFileName;
+    private static final double G = 6.67e-11;
+
+    public Planet(double xP, double yP, double xV,
+            double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.sqrt((xxPos - p.xxPos) * (xxPos - p.xxPos) + (yyPos - p.yyPos) * (yyPos - p.yyPos));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = calcDistance(p);
+        return G * mass * p.mass / (r * r);
+    }
+}
